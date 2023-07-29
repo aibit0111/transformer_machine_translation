0ba46d1c4a8f42d9598570d26c28b5ac4e14eaa8
Mohit <vegetasama0.88@gmail.com>
2023-07-29 13:39:31 +0530

    Update seq_seq.py

--- seq_seq.py
+++ seq_seq.py
@@ -13,7 +13,7 @@ from torchtext.datasets import WikiText2
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
  
-import copy
+import copy   
 import time
  
 class TransformerModel(nn.Module):
